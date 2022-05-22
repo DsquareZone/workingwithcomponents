e72eecc296be5e03ec0955cdca28cd5e750d125a
Surya Prakash <97003032+DsquareZone@users.noreply.github.com>
2022-05-22 19:43:14 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+import React from "react";
+import Person from "./person/Person";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <Person />
+    </div>
+  );
+}
---
+++ src/person/Person.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const person = () => {
+  return <h1> This is from person component </h1>;
+};
+export default person;
